acc1c0c231226e4a02407f856d18e6b9a5357b50
luanmcarvalho <luanmcarvalho@hotmail.com>
2025-02-28 22:01:34 -0300

    Animation projects card icon fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home () {
       id: 9, 
       title: 'Skol | Sinuquinha x Snooker', 
       categories: ['Compositing'], 
-      img: '/images/wizard-thumb.jpg', 
+      img: '/images/skol.jpg', 
       videoUrl: 'https://vimeo.com/835112131' 
     },
   ];
@@ -342,7 +342,7 @@ export default function Home () {
                   
                   <div className="bg-white text-black rounded-full w-10 h-10 mr-3 flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
                     </svg>
                   </div>
                 </div>
@@ -596,11 +596,10 @@ export default function Home () {
                     method="POST" 
                     className='space-y-4'
                     onSubmit={(e) => {
-                      e.preventDefault(); // Add this to prevent the default form submission
+                      e.preventDefault(); 
                       setFormSubmitting(true);
-                      // Simulate form submission
                       setTimeout(() => {
-                        setFormSubmitted(true); // Now we're using the state setter
+                        setFormSubmitted(true); 
                         setFormSubmitting(false);
                       }, 2000);
                     }}
