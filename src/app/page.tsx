7bbac96b8f8c6d3d87c8bae4543cb8e7616630ba
luanmcarvalho <luanmcarvalho@hotmail.com>
2025-03-30 01:18:04 -0300

    ultrah2O website added to portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -406,10 +406,10 @@ export default function Home () {
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-10">
             <div className="relative group cursor-pointer">
-              <a href="https://luanmcarvalho.github.io/airbnb-clone/" target="_blank" rel="noopener noreferrer" className="block">
+              <a href="https://ultrah2o.netlify.app/" target="_blank" rel="noopener noreferrer" className="block">
                 <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 group-hover:opacity-80 transition-opacity duration-300 z-10" />
-                <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20 font-organica">Airbnb Clone</h2>
-                <Image src="/images/airbnb-clone.png" alt="Airbnb Clone" width={800} height={453} className="w-full h-auto shadow-lg" />
+                <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20 font-organica">Ultra H2O</h2>
+                <Image src="/images/ULTRAH2O.png" alt="Ultra H2O" width={800} height={453} className="w-full h-auto shadow-lg" />
               </a>
             </div>
 
