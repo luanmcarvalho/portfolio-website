3b15c05465c8f63e20c74f7a2168f8d86b84f9f4
luanmcarvalho <luanmcarvalho@hotmail.com>
2025-11-26 09:27:03 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -564,7 +564,7 @@ export default function Home () {
                     <svg className="w-5 h-5 text-gray-400 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                     </svg>
-                    <span>+55 (11) 93258-9315</span>
+                    <span>+55 (91) 99158-9315</span>
                   </div>
                 </div>
                 
@@ -679,7 +679,7 @@ export default function Home () {
                 <svg className="w-5 h-5 mr-2 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                +55 (11) 93258-9315
+                +55 (91) 99158-9315
               </p>
             </div>
 
